ff4461997aa26c2d3403927c70455fc69a26cf25
Mario Mancilla <mariormt17@gmail.com>
2021-07-16 11:38:58 -0500

    Add the type Book to the query type field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,14 +4,9 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :books, [Types::BookType], null: false, description: "Returns all the books"
+    def books
+      Book.all
     end
   end
 end
